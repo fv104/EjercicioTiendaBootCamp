f87e46d9b7f90e6031dbddc1367af767a1bed9a5
Felix <fvicenta@capgemini.com>
2022-02-08 10:12:33 +0100

    Antes del checkout

--- store/src/app/components/product/product.component.ts
+++ store/src/app/components/product/product.component.ts
@@ -1,11 +1,12 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Product } from 'src/app/interfaces/product';
 
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
+  changeDetection : ChangeDetectionStrategy.OnPush
 })
 export class ProductComponent implements OnInit {
 
